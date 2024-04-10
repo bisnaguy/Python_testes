616407c747e0e0b3592dd2678f149a4958fcbf02
bisnaguy <azambujagabriel95@gmail.com>
2024-04-10 17:15:22 -0300

    Segundo commit

---
+++ Hora_salario.py
@@ -0,0 +1,7 @@
+number = int(input())
+worked_hour = int(input())
+hour_gain = float(input())
+total = worked_hour * hour_gain
+
+print(f"NUMBER = {number}")
+print(f"SALARY = U$ {total:.2f}")
--- Salario.py
+++ Salario.py
@@ -1,7 +1,7 @@
-nome = str(input("Informe o nome do vendedor: "))
-salario = float(input("Informe o salário do vendedor: "))
-vendas = float(input("Informe a quantidade de vendas desse vendedor: "))
+nome = str(input())
+salario = float(input())
+vendas = float(input())
 
 receber = salario + (vendas * 0.15)
 
-print(f"O vendedor {nome} tem o salário fixo de  {salario:.2f} junto a comissão de 15% sobre {vendas:.2f} acabou por receber o total de {round(receber, 2):.2f}")
+print(f"TOTAL = R$ {round(receber, 2):.2f}")
